0ab3a5f16b9757d63943c914f72187d3fbc5a0c8
Raymond Doetjes <rdoetjes@phonax.com>
2023-04-08 14:46:58 +0200

    mem in bars

--- public/htop.js
+++ public/htop.js
@@ -5,16 +5,26 @@ const html = htm.bind(h);
 
 function App(props) {
     return html`
-  <div class="memory">total memory: ${(props.sysinfo.tot_memory / (1024 * 1024 * 1024)).toFixed(2)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)} GB</div>
-  <div class="memory">total swap: ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_swap / (1024 * 1024 * 1024)).toFixed(2)} GB</div>
-  ${props.sysinfo.cpu_util.map(cpu => {
-        return html`<div class="bar">
-            <div class="bar-inner" style="width: ${cpu}%"></div>
-                <label>${cpu.toFixed(0)}%</label>
-            </div>`;
-    })}
-  });  
-  `;
+        <div class="bar">
+            <div class="bar-inner" style="width: ${(props.sysinfo.used_memory / props.sysinfo.tot_memory)*100}%">
+                <label>total ram: ${props.sysinfo.tot_memory / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+            </div>
+        </div>
+
+        <div class="bar">
+            <div class="bar-inner" style="width: ${(props.sysinfo.used_swap / props.sysinfo.tot_swap)*100}%">
+                <label>total swap: ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_swap / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+            </div>
+        </div>
+
+    ${props.sysinfo.cpu_util.map(cpu => {
+                return html`<div class="bar">
+                    <div class="bar-inner" style="width: ${cpu}%"></div>
+                        <label>${cpu.toFixed(0)}%</label>
+                    </div>`;
+            })}
+        });  
+        `;
 }
 
 let i = 0;
