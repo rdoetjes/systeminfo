9640655f91e46a297822003021acd294f40aa267
Raymond Doetjes <rdoetjes@phonax.com>
2023-04-08 15:18:02 +0200

    clean up

--- public/htop.js
+++ public/htop.js
@@ -4,27 +4,47 @@ import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
 function App(props) {
-    return html`
-        <div class="bar">
-            <div class="bar-inner" style="width: ${(props.sysinfo.used_memory / props.sysinfo.tot_memory)*100}%">
-                <label>total ram: ${props.sysinfo.tot_memory / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+    if (props.sysinfo.tot_swap == 0)
+        return html`
+            <div class="bar">
+                <div class="bar-inner" style="width: ${(props.sysinfo.used_memory / props.sysinfo.tot_memory)*100}%"></div>
+                <label>PHYSICAL RAM: ${props.sysinfo.tot_memory / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)}GB</label>
             </div>
-        </div>
-
-        <div class="bar">
-            <div class="bar-inner" style="width: ${(props.sysinfo.used_swap / props.sysinfo.tot_swap)*100}%">
-                <label>total swap: ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_swap / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+            
+            <div class="bar">
+                <div class="bar-inner" style="width:100%; background-color: #ccca45"></div>
+                <label>SWAP: NOT AVAILABLE</label>
             </div>
-        </div>
 
-    ${props.sysinfo.cpu_util.map(cpu => {
-                return html`<div class="bar">
-                    <div class="bar-inner" style="width: ${cpu}%"></div>
-                        <label>${cpu.toFixed(0)}%</label>
-                    </div>`;
-            })}
-        });  
-        `;
+        ${props.sysinfo.cpu_util.map(cpu => {
+                    return html`<div class="bar">
+                        <div class="bar-inner" style="width: ${cpu}%"></div>
+                            <label>${cpu.toFixed(0)}%</label>
+                        </div>`;
+                })}
+            });  
+            `;
+    else {
+        return html`
+            <div class="bar">
+                <div class="bar-inner" style="width: ${(props.sysinfo.used_memory / props.sysinfo.tot_memory)*100}%"></div>
+                <label>PHYSICAL RAM: ${props.sysinfo.tot_memory / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+            </div>
+            
+            <div class="bar">
+                <div class="bar-inner" style="width: ${(props.sysinfo.used_swap / props.sysinfo.tot_swap)*100}%"></div>
+                <label>SWAP: ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_swap / (1024 * 1024 * 1024)).toFixed(2)} GB</label>
+            </div>
+
+        ${props.sysinfo.cpu_util.map(cpu => {
+                    return html`<div class="bar">
+                        <div class="bar-inner" style="width: ${cpu}%"></div>
+                            <label>${cpu.toFixed(0)}%</label>
+                        </div>`;
+                })}
+            });  
+            `;
+    }
 }
 
 let i = 0;
