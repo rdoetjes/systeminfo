d7b47934fc25049fc247a5a4f674243dc62b78c8
Raymond Doetjes <rdoetjes@phonax.com>
2023-04-08 14:03:59 +0200

    web top

--- public/htop.js
+++ public/htop.js
@@ -5,12 +5,12 @@ const html = htm.bind(h);
 
 function App(props) {
     return html`
-  <div class="memory">memory ${props.sysinfo.tot_memory / (1024 * 1024 * 1024)} GB</div>
-  <div class="memory">swap ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)} GB</div>
+  <div class="memory">total memory: ${(props.sysinfo.tot_memory / (1024 * 1024 * 1024)).toFixed(2)}GB used: ${(props.sysinfo.used_memory / (1024 * 1024 * 1024)).toFixed(2)} GB</div>
+  <div class="memory">total swap: ${props.sysinfo.tot_swap / (1024 * 1024 * 1024)}GB used: ${(props.sysinfo.used_swap / (1024 * 1024 * 1024)).toFixed(2)} GB</div>
   ${props.sysinfo.cpu_util.map(cpu => {
         return html`<div class="bar">
             <div class="bar-inner" style="width: ${cpu}%"></div>
-            <label>${cpu.toFixed(2)}%</label>
+                <label>${cpu.toFixed(0)}%</label>
             </div>`;
     })}
   });  
